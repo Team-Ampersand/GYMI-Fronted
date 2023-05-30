90cebfcd04527e6e7d90c7596d75d6be120a1254
Jeongmin <srm0609@naver.com>
2023-05-30 20:14:33 +0900

    Create: GauthLoginButton

---
+++ src/components/SignIn/atom/GauthLoginButton/index.tsx
@@ -0,0 +1,29 @@
+import styled from '@emotion/styled';
+import GauthLogo from 'asset/svg/GauthLogo.svg';
+
+const GauthLoginButton = ({ onClick }: { onClick: () => void }) => {
+  return (
+    <Button onClick={onClick}>
+      <GauthLogo />
+      Sign in with GAuth
+    </Button>
+  );
+};
+
+const Button = styled.button`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 0.6rem;
+  padding: 0.875rem 4.5rem;
+  background: #2e80cc;
+  color: #fff;
+  font-size: 1rem;
+  box-shadow: 0 4px 15px rgba(0, 0, 0, 0.1);
+  border: none;
+  border-radius: 0.5rem;
+  cursor: pointer;
+`;
+
+export default GauthLoginButton;
